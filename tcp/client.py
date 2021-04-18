493d70ba1474c47d9ea1c65dc06d35e6398c95ce
Ivan :) <82706309+IGG01@users.noreply.github.com>
2021-04-18 15:26:29 +0200

    Create client.py

---
+++ tcp/client.py
@@ -0,0 +1,16 @@
+import socket
+
+target_host = "10.10.10.10"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Start the connection
+
+client.connect((socket.gethostname(), 80))
+
+client.send(bytes("GET /HTTP/1.1\nHost: google.com", "utf-8"))
+
+response = client.recv(1024)
+
+print(response)
